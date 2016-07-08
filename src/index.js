af47f9c1e84ef412a3c81376986c77db22a6ee8a
Christopher Duke <cd.duke@gmail.com>
2016-07-08 09:02:14 -0500

    starting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+export default function(marvel) {
+  fetch(`http://gateway.marvel.com:80/v1/public/series/9856?apikey=c4918d145e097447e98576b7c57449f8`)
+    .then((response) => response.json())
+    .then((info) => {
+
+    });
+}
