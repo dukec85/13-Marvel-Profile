9d6bb48bb8ed08774331f52df6e60745438ee4bb
Christopher Duke <cd.duke@gmail.com>
2016-07-11 17:36:11 -0500

    attempted JS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
-export default function(marvel) {
-  fetch(`http://gateway.marvel.com:80/v1/public/series/9856?apikey=c4918d145e097447e98576b7c57449f8`)
-    .then((response) => response.json())
-    .then((info) => {
+import SeriesInfoView from 'series-info-view';
 
+export default function(element) {
+
+  fetch(`http://gateway.marvel.com:80/v1/public/series/9856?apikey=c4918d145e097447e98576b7c57449f8`)
+    .then((res) => res.json())
+    .then((data) => {
+
+      const x = new SeriesInfoView(element, data);
+      x.render();
     });
 }
---
+++ src/series-info-view.js
@@ -0,0 +1,13 @@
+export default class SeriesInfoView {
+  constructor(element, data) {
+    this.element = element;
+    this.data = data;
+    console.log(data);
+
+  }
+
+  render() {
+    let image = this.element.querySelector('.character');
+    image.src = this.data.data.results[0].thumbnail.path + '.jpg';
+  }
+}
