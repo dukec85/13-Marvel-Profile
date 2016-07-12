52b2ef41a9745f853b3051942d4026eb7daece9d
Christopher Duke <cd.duke@gmail.com>
2016-07-12 04:09:01 -0500

    minor JS changes

--- src/series-info-view.js
+++ src/series-info-view.js
@@ -1,13 +1,13 @@
+  'use strict';
 export default class SeriesInfoView {
   constructor(element, data) {
     this.element = element;
     this.data = data;
-    console.log(data);
 
   }
 
   render() {
-    let image = this.element.querySelector('.character');
+    let image = this.element.querySelector('.stuff');
     image.src = this.data.data.results[0].thumbnail.path + '.jpg';
   }
 }
